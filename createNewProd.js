9ce6dec37fa7074ac0477b8240d395bbe05e662d
Dinelle Zipser <dinellezipser@gmail.com>
2022-06-26 14:20:22 -0700

    refactored updated to iterate and update items

--- createNewProd.js
+++ createNewProd.js
@@ -11,19 +11,22 @@ module.exports.createNewProd = async (sourceItem, destItem, excludeKeys) => {
     let newItem = await {
         ...sourceItem
     }
-    // justin's spaghetti
-    // for (let i=0;i<excludeKeys.length;i++) {
-    //     newItem.`${excludeKeys[i]}` = destItem.excludeKeys[i];
-    // }
-    //
-    for (let [key, value] of Object.entries(newItem)) {
-        for (let i = 0; i < excludeKeys.length; i++) {
-            if (key === excludeKeys[i]) {
-            newItem[excludeKeys[i]] = destItem[excludeKeys[i]];
-            console.log("Replacing: ",key);
-            }
-        } 
-    } 
-    console.log("createNewProd: ", newItem);
+
+    // for (let [key, value] of Object.entries(newItem)) {
+    //     for (let i = 0; i < excludeKeys.length; i++) {
+    //         if (key === excludeKeys[i]) {
+    //         newItem[excludeKeys[i]] = destItem[excludeKeys[i]];
+    //         console.log("Replacing: ",key);
+    //         }
+    //     } 
+    // } 
+
+    for (let i = 0; i < excludeKeys.length; i++) {
+        eval('newItem.' + excludeKeys[i] + '=' + 'destItem.' + excludeKeys[i]);
+      }
+
+    // console.log("createNewProd: ", newItem);
     return newItem;
+
+    // item === "common" ? console.log(excludeKeys['common']) : console.log(excludeKeys['games'])
 }
--- refactored.js
+++ refactored.js
@@ -1,9 +1,12 @@
 "use strict";
 
+// const _ = require("lodash");
+
 const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
 const { createNewProd } = require("./createNewProd");
 const { deleteOldProd } = require("./deleteOldProd");
 const { getddb } = require("./getddb");
+const { iterateAndUpdate } = require("./iterateAndUpdate");
 const { postNewProd } = require("./postNewProd");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -11,53 +14,85 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.run = async (event, context, callback) => {
   const sourceStage = event.pathParameters.sourceStage;
   const destStage = event.pathParameters.destStage;
-  
-  const item = "common";
-  const excludeKeys = [
-    "app.screens_v2.login.logo_url",
-    "app.screens_v2.menu.logo_url",
-    "app.screens_v2.signup.logo_url",
-    "game_api_stage",
-    "min_version",
-    "zoom",
-    "min_version",
-    "game_api",
-    "parameters_api_key",
-    "zoom_client_id"
+
+  const item = [
+    "CT",
+    "DW20",
+    "SK",
+    "VB",
+    "OCH",
+    "CHPT",
+    "onboarding",
+    "games",
+    "BNG",
+    "GAH",
+    "FLUS",
+    "SBS",
+    "audio",
+    "TW",
+    "common",
+    "TOT",
   ];
 
-  const sourceParams = {
-    TableName: `params-${sourceStage}`,
-    Key: {
-      parameter_set: item,
-    },
+  const excludeKeys = {
+    common: [
+      "app.screens_v2.login.logo_url",
+      "app.screens_v2.menu.logo_url",
+      "app.screens_v2.signup.logo_url",
+      "game_api_stage",
+      "min_version",
+      "zoom",
+      "min_version",
+      "game_api",
+      "parameters_api_key",
+      "zoom_client_id",
+      "chargebee_url",
+    ],
+    games: ["game_library.game_categories", "game_library.hero"],
   };
 
-  const destParams = {
-    TableName: `params-${destStage}`,
-    Key: {
-      parameter_set: item,
-    },
-  };
+  // iterateAndUpdate(item, sourceStage, destStage, excludeKeys);
+  for (let i = 0; i < item.length; i++) {
+    item[i];
 
-  // GET SOURCE AND PROD ITEMS
-  const sourceItem = await getddb(sourceParams);
+    const sourceParams = {
+      TableName: `params-${sourceStage}`,
+      Key: {
+        parameter_set: item[i],
+      },
+    };
 
-  console.log('sourceItem:', sourceItem);
+    const destParams = {
+      TableName: `params-${destStage}`,
+      Key: {
+        parameter_set: item[i],
+      },
+    };
 
-  const destItem = await getddb(destParams);
+    const sourceItem = await getddb(sourceParams);
+    const destItem = await getddb(destParams);
 
-  console.log("destItem:", destItem);
+    // console.log(item[i]) // prints each item
+    // console.log('item excludeKeys for:', excludeKeys[item[i]]) // 'undefined' for all excet 'games' and 'common
 
-// CREATE NEW PROD ITEM
-  const newItem = await createNewProd(sourceItem, destItem, excludeKeys);
+    if (excludeKeys[item[i]]) {
+      const newItem = await createNewProd(sourceItem, destItem, excludeKeys[item[i]]);
+      (console.log(JSON.stringify(newItem, null, 3)))
+      await deleteOldProd(destStage, item[i]);
+      await postNewProd(destStage, item[i], newItem);
+    
+    } else {
+      // const newItem = await sourceItem;
+      // console.log(`sourceItem of ${item[i]} copied`)
+      console.log(`sourceItem for ${item[i]} to be copied`)
+    }
 
-  console.log("refactored - newItem: ", newItem)
-
-//DELETE OLD PROD
-  await deleteOldProd(destStage, item);
-
-//POST NEW PROD ITEM TO DDB
-  await postNewProd(destStage, item, newItem);
-
+    // if (_.isEqual(newItem, destItem)) {
+    //   console.log(item[i] + " matches, no update needed.");
+    // } else {
+    //   await deleteOldProd(destStage, item[i]);
+    //   await postNewProd(destStage, item[i], newItem);
+    // }
+  }
+  
 };
